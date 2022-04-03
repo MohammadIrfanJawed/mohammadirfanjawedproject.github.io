f0e4bfe6b19e506ced8e9d1449a50c74bcfe269e
Mohammad Irfan Jawed <102671649+MohammadIrfanJawed@users.noreply.github.com>
2022-04-03 17:05:14 -0300

    MohammadPPproject

---
+++ script.js
@@ -0,0 +1,34 @@
+const itemcode = document.getElementById('itemcode')
+console.log(itemcode.value)
+const price = document.getElementById('price')
+console.log(price.value)
+
+const firstname = document.getElementById('firstname')
+console.log(firstname.value)
+
+const lastname = document.getElementById('lastname')
+console.log(lastname.value)
+
+const email = document.getElementById('email')
+console.log(email.value)
+
+const phonenumber = document.getElementById('phonenumber')
+console.log(phonenumber.value)
+
+const houseno = document.getElementById('houseno')
+console.log(houseno.value)
+
+const street = document.getElementById('street')
+console.log(street.value)
+
+const city = document.getElementById('city')
+console.log(city.value)
+
+const state = document.getElementById('state')
+console.log(state.value)
+
+const zip = document.getElementById('zip')
+console.log(zip.value)
+
+const country = document.getElementById('country')
+console.log(country.value)
